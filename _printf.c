3c6613505d0ddc387673f36da1b4ff312fa22df0
Ahmed-Ibrahim-Oueslati <oueslatiahmed22@gmail.com>
2024-08-15 16:00:02 +0100

    second tes

--- _printf.c
+++ _printf.c
@@ -1,14 +1,5 @@
 #include "main.h"
 /**
-* _putchar - Prints a character to the console.
-* @c: The character to print.
-* Return: Number of characters printed.
-*/
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-/**
 * _printf - Prints arguments based on a format string.
 * @format: The format string.
 * @...: The arguments to print.
