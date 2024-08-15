2d602a0be3da2d4d9b91919deb720290c1994fb3
Ahmed-Ibrahim-Oueslati <oueslatiahmed22@gmail.com>
2024-08-15 13:01:30 +0100

    _printf

--- _printf.c
+++ _printf.c
@@ -1,14 +1,56 @@
-#include <stdlib.h>
 #include "main.h"
-#include <stdio.h>
 /**
- * main - Entry point of the program.
- *
- * Description: Prints a string to the console.
- *
- * Return: Always 0 (success).
- */
-int main(void)
+* print_all - Prints various types of arguments based on a format string.
+*
+* @format: A string representing the types of arguments passed.
+*          'c' for char, 'i' for int, 'f' for float, 's' for string.
+* @...: A variable number of arguments.
+* Return: void.
+*/
+
+
+int _putchar(char c)
 {
-return (0);
+	return (write(1, &c, 1));
+}
+
+int _printf(const char *format, ...)
+{
+va_list ap;
+int i, count, j;
+char *str;
+char c;
+va_start(ap, format);
+i = 0;
+j = 0;
+while (format && format[i])
+{
+switch (format[i])
+{
+case 'c':
+c = va_arg(ap, int);
+_putchar(c);
+count = 0;
+break;
+case 's':
+str = va_arg(ap, char *);
+if (str == NULL)
+str = "(nil)";
+while (str[j] != '\0')
+{
+_putchar(str[j]);
+j++;
+}
+count = 0;
+break;
+default:
+count = 1;
+break;
+}
+if (format[i + 1] != '\0' && count == 0)
+printf(", ");
+i++;
+}
+_putchar('\n');
+va_end(ap);
 }
