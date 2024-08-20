b444a7229782b05733551dc69186ec0402228160
Ahmed-Ibrahim-Oueslati <oueslatiahmed22@gmail.com>
2024-08-20 11:30:50 +0100

    checking

--- _printf.c
+++ _printf.c
@@ -1,57 +1,50 @@
 #include "main.h"
 /**
-* _printf - Prints arguments based on a format string.
-* @format: The format string.
-* @...: The arguments to print.
-* Return: Number of characters printed.
+* _printf - Prints various types of arguments based on a format string.
+* @format: A string representing the types of arguments passed.
+*          'c' for char, 'i' for int, 'f' for float, 's' for string.
+* @...: A variable number of arguments.
+* Return: nb of charaters printed.
 */
 int _printf(const char *format, ...)
 {
 va_list ap;
-int i = 0, j, nb = 0;
+int i, j, nb;
 char *str;
-if (format == NULL)
-return (-1);
+char c;
 va_start(ap, format);
+i = 0;
+j = 0;
+nb = 0;
 while (format && format[i])
 {
-if (format[i] == '%')
-{
-i++;
 switch (format[i])
 {
 case 'c':
-nb +=
-_putchar(va_arg(ap, int));
+c = va_arg(ap, int);
+_putchar(c);
+nb++;
+break;
+case '%':
+c = va_arg(ap, int);
+_putchar('%');
+nb++;
 break;
 case 's':
 str = va_arg(ap, char *);
-if (!str)
+if (str == NULL)
 str = "(nil)";
-j = 0;
-while (str[j])
-nb +=
-_putchar(str[j++]);
-break;
-case '%':
-nb +=
-_putchar('%');
-break;
-default:
-nb +=
-_putchar('%');
-nb +=
-_putchar(format[i]);
-break;
-}
-}
-else
+while (str[j] != '\0')
 {
-	nb +=
-	_putchar(format[i]);
+_putchar(str[j]);
+nb++;
+j++;
+}
+break;
 }
 i++;
 }
+_putchar('\n');
 va_end(ap);
 return (nb);
 }
