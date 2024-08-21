9618116303a12985614a78c5ec8369ec7f1115d6
Ahmed-Ibrahim-Oueslati <oueslatiahmed22@gmail.com>
2024-08-21 01:47:21 +0100

    test

--- _printf.c
+++ _printf.c
@@ -1,16 +1,42 @@
 #include "main.h"
 #define BUFFER_SIZE 1024
-/**
-* _printf - Prints various types of arguments based on a format string.
-* @format: A string representing the types of arguments passed.
-*          'c' for char, 's' for string, 'd' or 'i' for integers,
-*          'b' for binary, 'u' for unsigned, 'o' for octal,
-*          'x' for hex (lowercase), 'X' for hex (uppercase),
-*          'S' for string with non-printable characters as \xXX,
-*          'p' for pointer, and '%%' for a literal '%'.
-* @...: A variable number of arguments.
-* Return: Number of characters printed or -1 on error.
-*/
+
+int int_to_binary(unsigned int num, char *buffer, int buffer_size)
+{
+int i = buffer_size - 1;
+buffer[i] = '\0';
+while (i >= 0 && num > 0)
+{
+buffer[i] = (num % 2) + '0';
+num /= 2;
+i--;
+}
+return (i + 1);
+}
+int int_to_base(unsigned int num, char *buffer, int buffer_size, int base, int uppercase)
+{
+int i = buffer_size - 1;
+buffer[i] = '\0';
+while (i >= 0 && num > 0)
+{
+int digit = num % base;
+if (digit < 10)
+{
+	buffer[i] = digit + '0';
+} else
+{
+	buffer[i] = (digit - 10) + (uppercase ? 'A' : 'a');
+}
+num /= base;
+i--;
+}
+return (i + 1);
+}
+void flush_buffer(char *buffer, int buffer_index)
+{
+	write(1, buffer, buffer_index);
+}
+
 int _printf(const char *format, ...)
 {
 va_list ap;
@@ -28,7 +54,9 @@ int field_width = 0;
 int precision = -1;
 char pad_char = ' ';
 if (!format)
-return ((-1));
+{
+	return (-1);
+}
 va_start(ap, format);
 while (format[i])
 {
@@ -40,18 +68,20 @@ length_modifier = 0;
 field_width = 0;
 precision = -1;
 pad_char = ' ';
-
 while (format[i] == '+' || format[i] == ' ' || format[i] == '#')
 {
 if (format[i] == '+')
-flag_plus = 1;
-else if (format[i] == ' ')
-flag_space = 1;
-else if (format[i] == '#')
-flag_hash = 1;
+{
+	flag_plus = 1;
+} else if (format[i] == ' ')
+{
+	flag_space = 1;
+} else if (format[i] == '#')
+{
+	flag_hash = 1;
+}
 i++;
 }
-
 if (format[i] >= '0' && format[i] <= '9')
 {
 if (format[i] == '0')
@@ -65,7 +95,6 @@ field_width = field_width * 10 + (format[i] - '0');
 i++;
 }
 }
-
 if (format[i] == '.')
 {
 i++;
@@ -76,13 +105,11 @@ precision = precision * 10 + (format[i] - '0');
 i++;
 }
 }
-
 if (format[i] == 'l' || format[i] == 'h')
 {
 length_modifier = format[i];
 i++;
 }
-
 switch (format[i])
 {
 case 'c':
@@ -93,12 +120,18 @@ break;
 case 's':
 str = va_arg(ap, char *);
 if (str == NULL)
-str = "(nil)";
+{
+	str = "(nil)";
+}
 length = 0;
 while (str[length] != '\0')
-length++;
+{
+	length++;
+}
 if (precision >= 0 && precision < length)
-length = precision;
+{
+	length = precision;
+}
 if (length < field_width)
 {
 for (j = 0; j < field_width - length; j++)
@@ -118,17 +151,20 @@ break;
 case 'd':
 case 'i':
 if (length_modifier == 'l')
-num = va_arg(ap, long);
-else if (length_modifier == 'h')
-num = (short)va_arg(ap, int);
-else
-num = va_arg(ap, int);
+{
+	num = va_arg(ap, long);
+} else if (length_modifier == 'h')
+{
+	num = (short)va_arg(ap, int);
+} else
+{
+	num = va_arg(ap, int);
+}
 if (flag_plus && num >= 0)
 {
 buffer[buffer_index++] = '+';
 nb++;
-}
-else if (flag_space && num >= 0)
+} else if (flag_space && num >= 0)
 {
 buffer[buffer_index++] = ' ';
 nb++;
@@ -136,7 +172,9 @@ nb++;
 sprintf(temp_buffer, "%ld", num);
 length = 0;
 while (temp_buffer[length] != '\0')
-length++;
+{
+	length++;
+}
 if (precision > length)
 {
 for (j = 0; j < precision - length; j++)
@@ -144,8 +182,7 @@ for (j = 0; j < precision - length; j++)
 buffer[buffer_index++] = '0';
 nb++;
 }
-}
-else if (length < field_width)
+} else if (length < field_width)
 {
 for (j = 0; j < field_width - length; j++)
 {
@@ -180,15 +217,21 @@ nb++;
 break;
 case 'u':
 if (length_modifier == 'l')
-unsigned_num = va_arg(ap, unsigned long);
-else if (length_modifier == 'h')
-unsigned_num = (unsigned short)va_arg(ap, unsigned int);
-else
-unsigned_num = va_arg(ap, unsigned int);
+{
+	unsigned_num = va_arg(ap, unsigned long);
+} else if (length_modifier == 'h')
+{
+	unsigned_num = (unsigned short)va_arg(ap, unsigned int);
+} else
+{
+	unsigned_num = va_arg(ap, unsigned int);
+}
 sprintf(temp_buffer, "%lu", unsigned_num);
 length = 0;
 while (temp_buffer[length] != '\0')
-length++;
+{
+	length++;
+}
 if (precision > length)
 {
 for (j = 0; j < precision - length; j++)
@@ -196,8 +239,7 @@ for (j = 0; j < precision - length; j++)
 buffer[buffer_index++] = '0';
 nb++;
 }
-}
-else if (length < field_width)
+} else if (length < field_width)
 {
 for (j = 0; j < field_width - length; j++)
 {
@@ -215,11 +257,15 @@ j++;
 break;
 case 'o':
 if (length_modifier == 'l')
-unsigned_num = va_arg(ap, unsigned long);
-else if (length_modifier == 'h')
-unsigned_num = (unsigned short)va_arg(ap, unsigned int);
-else
-unsigned_num = va_arg(ap, unsigned int);
+{
+	unsigned_num = va_arg(ap, unsigned long);
+} else if (length_modifier == 'h')
+{
+	unsigned_num = (unsigned short)va_arg(ap, unsigned int);
+} else
+{
+	unsigned_num = va_arg(ap, unsigned int);
+}
 if (flag_hash && unsigned_num != 0)
 {
 buffer[buffer_index++] = '0';
@@ -233,8 +279,7 @@ for (j = 0; j < precision - length; j++)
 buffer[buffer_index++] = '0';
 nb++;
 }
-}
-else if (length < field_width)
+} else if (length < field_width)
 {
 for (j = 0; j < field_width - length; j++)
 {
@@ -250,11 +295,15 @@ nb++;
 break;
 case 'x':
 if (length_modifier == 'l')
-unsigned_num = va_arg(ap, unsigned long);
-else if (length_modifier == 'h')
-unsigned_num = (unsigned short)va_arg(ap, unsigned int);
-else
-unsigned_num = va_arg(ap, unsigned int);
+{
+	unsigned_num = va_arg(ap, unsigned long);
+} else if (length_modifier == 'h')
+{
+	unsigned_num = (unsigned short)va_arg(ap, unsigned int);
+} else
+{
+	unsigned_num = va_arg(ap, unsigned int);
+}
 if (flag_hash && unsigned_num != 0)
 {
 buffer[buffer_index++] = '0';
@@ -269,8 +318,7 @@ for (j = 0; j < precision - length; j++)
 buffer[buffer_index++] = '0';
 nb++;
 }
-}
-else if (length < field_width)
+} else if (length < field_width)
 {
 for (j = 0; j < field_width - length; j++)
 {
@@ -286,11 +334,15 @@ nb++;
 break;
 case 'X':
 if (length_modifier == 'l')
-unsigned_num = va_arg(ap, unsigned long);
-else if (length_modifier == 'h')
-unsigned_num = (unsigned short)va_arg(ap, unsigned int);
-else
-unsigned_num = va_arg(ap, unsigned int);
+{
+	unsigned_num = va_arg(ap, unsigned long);
+} else if (length_modifier == 'h')
+{
+	unsigned_num = (unsigned short)va_arg(ap, unsigned int);
+} else
+{
+	unsigned_num = va_arg(ap, unsigned int);
+}
 if (flag_hash && unsigned_num != 0)
 {
 buffer[buffer_index++] = '0';
@@ -305,8 +357,7 @@ for (j = 0; j < precision - length; j++)
 buffer[buffer_index++] = '0';
 nb++;
 }
-}
-else if (length < field_width)
+} else if (length < field_width)
 {
 for (j = 0; j < field_width - length; j++)
 {
@@ -327,7 +378,9 @@ if (ptr == NULL)
 str = "(nil)";
 length = 0;
 while (str[length] != '\0')
-length++;
+{
+	length++;
+}
 if (length < field_width)
 {
 for (j = 0; j < field_width - length; j++)
@@ -343,15 +396,16 @@ buffer[buffer_index++] = str[j];
 nb++;
 j++;
 }
-}
-else
+} else
 {
 buffer[buffer_index++] = '0';
 buffer[buffer_index++] = 'x';
 sprintf(temp_buffer, "%lx", (unsigned long)ptr);
 length = 0;
 while (temp_buffer[length] != '\0')
-length++;
+{
+	length++;
+}
 if (length + 2 < field_width)
 {
 for (j = 0; j < field_width - (length + 2); j++)
@@ -380,8 +434,7 @@ buffer[buffer_index++] = format[i];
 nb += 2;
 break;
 }
-}
-else
+} else
 {
 buffer[buffer_index++] = format[i];
 nb++;
@@ -394,7 +447,8 @@ buffer_index = 0;
 i++;
 }
 if (buffer_index > 0)
-flush_buffer(buffer, buffer_index);
-va_end(ap);
-return ((nb));
+{
+	flush_buffer(buffer, buffer_index);
 }
+va_end(ap);
+return (nb);
